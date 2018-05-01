2b5a352cd118c4fc2535e1ee1f28c1459469b09e
Evgeny Belyavsky <belya0695@mail.ru>
2018-05-01 15:21:20 +0300

    Added redirect to error page

--- EC/EC.Web/Controllers/HomeController.cs
+++ EC/EC.Web/Controllers/HomeController.cs
@@ -4,16 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EC.Common;
+using EC.Business;
 
 namespace EC.Web.Controllers
 {
     public class HomeController : Controller
     {
+        Logger logger = new Logger();
+
         // GET: Home
         public ActionResult Index()
         {
-            Logger logger = new Logger();
-            logger.LogInfo("я кажется работаю");
+            try
+            {                
+                logger.LogInfo("я кажется работаю");
+                StacktraseTest.TestMethod();
+            }
+            catch (ArgumentException ex)
+            {
+                logger.LogInfo(ex.Message);
+                return Redirect("/Content/Exceptions/500.html");
+            }
             return View();
         }
     }
