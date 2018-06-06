a4a78a9ad20b9c1a90aad1a7f3285d948d24ab31
Evgeny Belyavsky <belya0695@mail.ru>
2018-06-06 13:52:42 +0300

    master fix

---
+++ EC/EC.Business/StacktraseTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.Business
+{
+    public interface IStacktraseTest
+    {
+        void TestMethod();
+    }
+
+    public class StacktraseTest : IStacktraseTest
+    {
+        public void TestMethod()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
+
