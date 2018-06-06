6643d6dbcb95843ff80436c3e4d992254df43bb6
Evgeny Belyavsky <belya0695@mail.ru>
2018-06-06 22:18:58 +0300

    added auth attributes for doctor, editor and patient

--- EC/EC.Web/AuthAttributes/DoctorAttribute.cs
+++ EC/EC.Web/AuthAttributes/DoctorAttribute.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using EC.Web.AuthAttributes.Models;
 
 namespace EC.Web.AuthAttributes
 {
-    public class DoctorAttribute
+    public class DoctorAttribute : AuthorizeAttribute
     {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            UserPrincipal userPrincipal = HttpContext.Current.User as UserPrincipal;
+            if (userPrincipal != null)
+            {
+                if (!userPrincipal.IsInRole("doctor"))
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Denied", requiredRole = "Doctor" }));
+                }
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Login" }));
+            }
+        }
+
     }
 }
--- EC/EC.Web/AuthAttributes/PatientAttribute.cs
+++ EC/EC.Web/AuthAttributes/PatientAttribute.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using EC.Web.AuthAttributes.Models;
 
 namespace EC.Web.AuthAttributes
 {
-    public class PatientAttribute
+    public class PatientAttribute : AuthorizeAttribute
     {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            UserPrincipal userPrincipal = HttpContext.Current.User as UserPrincipal;
+            if (userPrincipal != null)
+            {
+                if (!userPrincipal.IsInRole("patient"))
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Denied", requiredRole = "Patient" }));
+                }
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Login" }));
+            }
+        }
     }
 }
