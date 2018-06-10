a4beec4507404ddd04fa9a808f7c8f74f4bc9ad5
Evgeny Belyavsky <belya0695@mail.ru>
2018-06-10 15:23:01 +0300

    fixed stored proc GetProceduresListsByRecordId

--- Database/storedProcedures/GetProceduresListsByRecordId.sql
+++ Database/storedProcedures/GetProceduresListsByRecordId.sql
@@ -1,7 +1,7 @@
 USE [EC.DB]
 GO
 
-/****** Object:  StoredProcedure [dbo].[GetProceduresListsByRecordId]    Script Date: 09.06.2018 19:35:54 ******/
+/****** Object:  StoredProcedure [dbo].[GetProceduresListsByRecordId]    Script Date: 10.06.2018 15:21:57 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,8 +12,8 @@ CREATE PROCEDURE [dbo].[GetProceduresListsByRecordId]
 @record_id int
 AS
 BEGIN
-SELECT *
-FROM dbo.procedures_lists
+SELECT *, dbo.medical_procedures.procedure_name
+FROM dbo.procedures_lists INNER JOIN dbo.medical_procedures ON dbo.procedures_lists.procedure_id = dbo.medical_procedures.procedure_id
 WHERE dbo.procedures_lists.record_id = @record_id
 END
 GO
